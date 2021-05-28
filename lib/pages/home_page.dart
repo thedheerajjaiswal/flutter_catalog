aa89be7f18b70ab121aa6701123b0a25a8071d27
thedheerajjaiswal <dheerajjaiswal0781@gmail.com>
2021-05-29 01:12:29 +0530

    pages commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dating App'),
+        title: Text('Learning App'),
       ),
       body: Center(
         child: Container(
