74904448f1221d171f48097842851f7efb1bc89b
thedheerajjaiswal <dheerajjaiswal0781@gmail.com>
2021-05-29 01:05:07 +0530

    second commit

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int myAge = 26;
+  final String myName = 'Dheeraj Jaiswal';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dating App'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('My name is $myName  & i am $myAge year old !'),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
